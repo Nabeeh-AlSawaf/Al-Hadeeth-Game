59e285776a61acec217193098a704e23845c64ff
Nabeeh Sawaf <nabeeh.alsawaf@gmail.com>
2020-08-13 05:26:30 +0300

    Menus Added

---
+++ HadeethGame/Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables  : MonoBehaviour
+{
+    public static Settings settings;
+    public static int HadeethNumber;
+
+}
