8603745103b42428dbf4140171d8a7316cdbb645
BigBoss <miias.hubi@gmail.com>
2020-08-14 22:13:36 +0300

    added character controller

---
+++ HadeethGame/Assets/Scripts/MVC/Controlle/C_CharacterControll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_CharacterControll : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Joystick joystick;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+
+    private float turnSmoothVelocity;
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = joystick.Horizontal;
+        float vertical = joystick.Vertical;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
