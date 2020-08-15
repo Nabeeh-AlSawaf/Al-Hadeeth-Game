75b275eed396004efc8278896422d8c8bfc43987
BigBoss <miias.hubi@gmail.com>
2020-08-15 18:10:54 +0300

    camera and character controller added to forest scene

--- HadeethGame/Assets/Scripts/MVC/Controlle/C_CharacterControll.cs
+++ HadeethGame/Assets/Scripts/MVC/Controlle/C_CharacterControll.cs
@@ -5,28 +5,85 @@ using UnityEngine;
 public class C_CharacterControll : MonoBehaviour
 {
     public CharacterController controller;
-
     public Joystick joystick;
 
     public float speed = 6f;
-
     public float turnSmoothTime = 0.1f;
 
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     private float turnSmoothVelocity;
+
+    private Vector3 playerDirection;
+    private Quaternion newAngle;
+    private Vector3 velocity;
+    private bool isGrounded;
     // Update is called once per frame
-    void Update()
+
+    private void ReadInput()
     {
         float horizontal = joystick.Horizontal;
         float vertical = joystick.Vertical;
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        if(direction.magnitude >= 0.1f)
+        playerDirection = new Vector3(horizontal, 0f, vertical);
+    }
+
+    private void CalculateMovementPosition()
+    {
+        if (playerDirection.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(playerDirection.x, playerDirection.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            newAngle = Quaternion.Euler(0f, angle, 0f);
 
-            controller.Move(direction * speed * Time.deltaTime);
+
+            //controller.Move(playerDirection * speed * Time.deltaTime);
         }
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+    }
+
+    private void CalculateGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+    private void ApplyGravity()
+    {
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void ResetVelocity()
+    {
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    private void Move()
+    {
+            transform.rotation = newAngle;
+            controller.Move(playerDirection * speed * Time.deltaTime);
+    }
+
+
+
+    void Update()
+    {
+        ReadInput();
+        CalculateMovementPosition();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        CalculateGravity();
+        ApplyGravity();
+        ResetVelocity();
     }
 }
