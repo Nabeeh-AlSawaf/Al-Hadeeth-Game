04071c7b75ef3ab55f77817055d01346ba47032f
BigBoss <miias.hubi@gmail.com>
2020-08-17 22:24:52 +0300

    added levels and more objects to the forest scene

---
+++ HadeethGame/Assets/Scripts/MVC/View/V_LevelActivation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V_LevelActivation : MonoBehaviour
+{
+
+    Animator animator;
+    bool activate=false;
+    public bool LastLevel = false;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        if (LastLevel) animator.SetTrigger("LastLevel");
+
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            if (!activate)
+            {
+                animator.SetTrigger("Activate");
+                activate = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            animator.SetTrigger("DeActivate");
+            activate = false;
+        }
+    }
+    
+}
